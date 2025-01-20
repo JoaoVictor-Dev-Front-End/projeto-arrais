33f273311019ccb00e5cceb6e1982b53ec82b993
unknown <joaosilva0074@digitalcollege.internal>
2025-01-20 12:04:50 -0300

    atualizacoes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,30 @@
+import { UserOutlined } from "@ant-design/icons"
+import { Button } from "antd";
+import { Input } from "antd";
+
+const Login = () => {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <form className="w-1/4 p-4 border rounded-md">
+                <Input
+                    placeholder="email@email.com"
+                    prefix={
+                        <UserOutlined className="text-black/25" />
+                    }
+                />
+                <Input.Password
+                    className="mb-3"
+                    placeholder="input password"
+                />
+                <Button
+                    type="primary"
+                    className="!bg-laranja hover:!bg-laranja2"
+                >
+                    Entrar
+                </Button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
